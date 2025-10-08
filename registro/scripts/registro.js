d4f15cacebc5ad0bd87d08feda83f924b015f5ad
Alan <nacrma.mg@gmail.com>
2025-10-07 18:34:18 -0600

    ejercicio 3 de javascript resuelto

--- registro/scripts/registro.js
+++ registro/scripts/registro.js
@@ -1,35 +1,35 @@
 function verInformacion(){
-let i;
-    __________ ocupacion = "";
-    __________ pasatiempos = "";
-    const nombre = __________.querySelector('__________ [__________="__________"]');
-    const edad = __________.querySelector('__________ [__________="__________"]');
-    const estado = __________.querySelector('__________ [__________="__________"]');
-    const aOcupacion = __________.querySelectorAll('__________ [__________="__________"]');
-    const aPasatiempos = __________.querySelectorAll('__________ [__________="__________"]');
-    for (i = 0; i < __________.length; i++) {
+    let i;
+    let ocupacion = "";
+    let pasatiempos = "";
+    const nombre = document.querySelector('input[name="txt_nombre"]');
+    const edad = document.querySelector('input[name="nmbr_edad"]');
+    const estado = document.querySelector('select[name="slct_estado"]');
+    const aOcupacion = document.querySelectorAll('input[name="rd_ocupacion"]');
+    const aPasatiempos = document.querySelectorAll('input[name="chk_pasatiempo"]');
+    for (i = 0; i < aOcupacion.length; i++) {
         if (aOcupacion[i].checked) {
-            ocupacion = aOcupacion[i].__________;
+            ocupacion = aOcupacion[i].value;
             i = aOcupacion.length;
         }
     }
-    for (i = __________; i < __________.__________; __________) {
-        if (__________.__________) {
-            pasatiempos += __________.__________ + " ";
+    for (i = 0; i < aPasatiempos.length; i++) {
+        if (aPasatiempos[i].checked) {
+            pasatiempos += aPasatiempos[i].value + " ";
         }
     }
     let ventana = window.open("","vtnInformacion","width=300,height=200");
     ventana.document.open();
     ventana.document.writeln("<html><head><title>Datos capturados</title></head><body>");
     ventana.document.writeln("Nombre: " + nombre.value.toUpperCase() + "<br>");
-    ventana.document.writeln("Edad: " + __________.__________ + "<br>");
-    ventana.document.writeln("Estado civil: "+ estado.options[__________.__________].text + "<br>");
-    ventana.document.writeln("Ocupacion: " + __________ + "<br>");
-    ventana.document.writeln("Pasatiempos: " + __________ + "<br>");
+    ventana.document.writeln("Edad: " + edad.value + "<br>");
+    ventana.document.writeln("Estado civil: "+ estado.options[estado.selectedIndex].text + "<br>");
+    ventana.document.writeln("Ocupacion: " + ocupacion + "<br>");
+    ventana.document.writeln("Pasatiempos: " + pasatiempos + "<br>");
     ventana.document.writeln("</body></html>");
     ventana.document.close();
     ventana.focus();
 }
 window.addEventListener('load', function(){
-    document.getElementById('__________').addEventListener('__________', __________);
+    document.getElementById('btn_enviar').addEventListener('click', verInformacion);
 });
